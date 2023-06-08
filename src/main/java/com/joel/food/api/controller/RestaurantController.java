dc075d1a480e76eace273446030fb261f1f11aea
Joel Maciel <soldado25461@gmail.com>
2023-06-08 00:55:20 -0300

    feat: creating restauranteService and refactoring restauranteController

--- src/main/java/com/joel/food/api/controller/RestaurantController.java
+++ src/main/java/com/joel/food/api/controller/RestaurantController.java
@@ -1,13 +1,15 @@
 package com.joel.food.api.controller;
 
+import com.joel.food.domain.exception.EntityNotExistsException;
+import com.joel.food.domain.model.Kitchen;
 import com.joel.food.domain.model.Restaurant;
+import com.joel.food.domain.repository.KitchenRepository;
 import com.joel.food.domain.repository.RestaurantRepository;
+import com.joel.food.domain.service.RestaurantRegistrationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +19,9 @@ import java.util.List;
 public class RestaurantController {
 
     private final RestaurantRepository restaurantRepository;
+
+    private final KitchenRepository kitchenRepository;
+    private final RestaurantRegistrationService restaurantService;
 
     @GetMapping
     public List<Restaurant> list() {
@@ -31,5 +36,15 @@ public class RestaurantController {
             return ResponseEntity.ok(restaurant);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?>  save(@RequestBody Restaurant restaurant) {
+        try {
+            restaurant = restaurantService.save(restaurant);
+            return ResponseEntity.status(HttpStatus.CREATED).body(restaurant);
+        } catch (EntityNotExistsException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
