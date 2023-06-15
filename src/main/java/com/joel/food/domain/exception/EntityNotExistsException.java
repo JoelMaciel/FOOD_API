c92b6978f40bf574629f0f91d1e737258496ca5e
Joel Maciel <soldado25461@gmail.com>
2023-06-15 16:09:34 -0300

    feat: handling kitchen exceptions and refactoring the controller

--- src/main/java/com/joel/food/domain/exception/EntityNotExistsException.java
+++ src/main/java/com/joel/food/domain/exception/EntityNotExistsException.java
@@ -1,5 +1,9 @@
 package com.joel.food.domain.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class EntityNotExistsException extends RuntimeException {
 
     public EntityNotExistsException(String message) {
--- src/main/java/com/joel/food/domain/service/KitchenRegistrationService.java
+++ src/main/java/com/joel/food/domain/service/KitchenRegistrationService.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class KitchenRegistrationService {
 
+    public static final String MSG_KITCHEN_NOT_FOUND = "There is no kitchen record with the code %d";
+    public static final String MSG_KITCHEN_IN_USE = "Kitchen code %d cannot be removed as it is in use";
     private final KitchenRepository kitchenRepository;
 
     public Kitchen save(Kitchen kitchen) {
@@ -24,10 +26,15 @@ public class KitchenRegistrationService {
             kitchenRepository.deleteById(kitchenId);
         } catch (EmptyResultDataAccessException e) {
             throw new EntityNotExistsException(
-                    String.format("There is no kitchen record with the code %d", kitchenId));
+                    String.format(MSG_KITCHEN_NOT_FOUND, kitchenId));
         } catch (DataIntegrityViolationException e) {
-            throw new EntityInUseException(
-                    String.format("Kitchen code %d cannot be removed as it is in use", kitchenId));
+            throw new EntityInUseException(String.format(MSG_KITCHEN_IN_USE, kitchenId));
         }
+    }
+
+    public Kitchen searchById(Long kitchenId) {
+        return kitchenRepository.findById(kitchenId)
+                .orElseThrow(() -> new EntityNotExistsException(
+                        String.format(MSG_KITCHEN_NOT_FOUND, kitchenId)));
     }
 }
