9e6a2d0d7cc415ac9858a5b743e0c3b3b667bd5d
Joel Maciel <soldado25461@gmail.com>
2023-06-18 20:20:05 -0300

    feat: Handling exceptions from missing or ignored properties

--- src/main/java/com/joel/food/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/joel/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,15 +1,23 @@
 package com.joel.food.api.exceptionhandler;
 
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 import com.joel.food.domain.exception.BusinessException;
 import com.joel.food.domain.exception.EntityInUseException;
 import com.joel.food.domain.exception.EntityNotExistsException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -29,15 +37,80 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EntityInUseException.class)
     public ResponseEntity<?> handleEntityInUse(EntityInUseException ex, WebRequest request) {
 
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
+        HttpStatus status = HttpStatus.CONFLICT;
+        ProblemType problemType = ProblemType.ENTITY_IN_USE;
+        String detail = ex.getMessage();
+
+        Problem problem = createProblemBuilder(status,problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<?> handleBusiness(BusinessException ex, WebRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ProblemType problemType = ProblemType.BUSINESS_ERROR;
+        String detail = ex.getMessage();
 
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 
     }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                 HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Throwable rootCause = ExceptionUtils.getRootCause(ex);
+
+        if( rootCause instanceof InvalidFormatException) {
+            return handleInvalidFormat((InvalidFormatException) rootCause, headers,status, request);
+        }else if (rootCause instanceof PropertyBindingException) {
+            return handlePropertyBinding((PropertyBindingException) rootCause, headers, status, request);
+        }
+
+        ProblemType problemType = ProblemType.MESSAGE_INCOMPREHESIBLE;
+        String detail = "The request body is invalid. Check syntax error";
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+    }
+
+    private ResponseEntity<Object> handleInvalidFormat(InvalidFormatException ex,
+                                HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        String path = joinPath(ex.getPath());
+
+        ProblemType problemType = ProblemType.MESSAGE_INCOMPREHESIBLE;
+        String detail = String.format("Property '%s' received value '%s', "
+                + "which is of an invalid type. Please correct and enter a value compatible with type %s " ,
+                path , ex.getValue() , ex.getTargetType().getSimpleName());
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, headers, status, request );
+    }
+
+    private ResponseEntity<Object>  handlePropertyBinding(PropertyBindingException ex, HttpHeaders headers,
+                                              HttpStatus status, WebRequest request) {
+        String path = joinPath(ex.getPath());
+
+        ProblemType problemType = ProblemType.MESSAGE_INCOMPREHESIBLE;
+        String detail = String.format("Property '%s' does not exist. "
+                + "Correct or remove this property and try again.", path);
+        Problem problem = createProblemBuilder(status, problemType,detail).build();
+
+        return handleExceptionInternal(ex, problem, headers,status,request);
+    }
+
+    private String joinPath(List<Reference> references) {
+        return references.stream()
+                .map(ref -> ref.getFieldName())
+                .collect(Collectors.joining("."));
+    }
+
 
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body,
--- src/main/java/com/joel/food/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/joel/food/api/exceptionhandler/ProblemType.java
@@ -5,7 +5,10 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
-    ENTITY_NOT_FOUND("/entity-not-found", "Entity not found");
+    MESSAGE_INCOMPREHESIBLE("/message-incomprehensible", "Message incomprehensible"),
+    ENTITY_NOT_FOUND("/entity-not-found", "Entity not found"),
+    ENTITY_IN_USE("/entity-in-use", "Entity in use"),
+    BUSINESS_ERROR("/businnes-error", "Business error");
 
     private String title;
     private String uri;
