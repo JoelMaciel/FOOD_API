6d89f329ec6614bbb5ff5aa01e04267eeecdb796
Joel Maciel <soldado25461@gmail.com>
2023-06-18 21:32:47 -0300

    feat: Handling uncaught exceptions, adding new properties to the Problem

--- src/main/java/com/joel/food/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/joel/food/api/exceptionhandler/ProblemType.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
+    SYSTEM_ERROR("/system-error", "System-error"),
+    INVALID_PARAMETER("/invalid-parameter", "Invalid parameter"),
     MESSAGE_INCOMPREHESIBLE("/message-incomprehensible", "Message incomprehensible"),
-    ENTITY_NOT_FOUND("/entity-not-found", "Entity not found"),
+    RESOURCE_NOT_FUND("/resource-not-found", "Resource not found"),
     ENTITY_IN_USE("/entity-in-use", "Entity in use"),
-    BUSINESS_ERROR("/businnes-error", "Business error");
+    BUSINESS_ERROR("/business-error", "Business error");
 
     private String title;
     private String uri;
