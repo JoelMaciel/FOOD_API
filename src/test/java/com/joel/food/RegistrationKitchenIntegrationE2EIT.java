ece8807f64ae5584161ac8ec5fb15b334a522423
Joel Maciel <soldado25461@gmail.com>
2023-06-20 19:33:01 -0300

    test:

--- src/test/java/com/joel/food/RegistrationKitchenIntegrationE2EIT.java
+++ src/test/java/com/joel/food/RegistrationKitchenIntegrationE2EIT.java
@@ -1,14 +1,17 @@
 package com.joel.food;
 
+import com.joel.food.domain.model.Kitchen;
+import com.joel.food.domain.repository.KitchenRepository;
+import com.joel.food.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
-import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItems;
@@ -16,20 +19,26 @@ import static org.hamcrest.Matchers.hasSize;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 class RegistrationKitchenIntegrationE2EIT {
 
     @LocalServerPort
     private int port;
 
     @Autowired
-    private Flyway flyway;
+    private DatabaseCleaner databaseCleaner;
+
+    @Autowired
+    private KitchenRepository kitchenRepository;
 
     @BeforeEach
     public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/kitchens";
-        flyway.migrate();
+
+        databaseCleaner.clearTables();
+        prepareData();
     }
 
 
@@ -45,14 +54,14 @@ class RegistrationKitchenIntegrationE2EIT {
     }
 
     @Test
-    public void mustContain4Kitchens_WhenConsultKitchen() {
+    public void mustContain2Kitchens_WhenConsultKitchen() {
                 given()
                      .accept(ContentType.JSON)
                 .when()
                      .get()
                 .then()
-                      .body("", hasSize(4))
-                      .body("name", hasItems("Indian", "Thai"));
+                      .body("", hasSize(2))
+                      .body("name", hasItems("Thai", "American"));
     }
 
     @Test
@@ -65,6 +74,17 @@ class RegistrationKitchenIntegrationE2EIT {
                 .post()
                 .then()
                 .statusCode(HttpStatus.SC_CREATED);
+
+    }
+
+    private void prepareData() {
+        Kitchen kitchen1 = new Kitchen();
+        kitchen1.setName("Thai");
+        kitchenRepository.save(kitchen1);
+
+        Kitchen kitchen2 = new Kitchen();
+        kitchen2.setName("American");
+        kitchenRepository.save(kitchen2);
 
     }
 }
