120e57d901ac3611303f73d991477f49be789f0e
Joel Maciel <soldado25461@gmail.com>
2023-06-21 22:41:21 -0300

    test: Restaurant E2E Tests

--- src/main/java/com/joel/food/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/joel/food/api/exceptionhandler/ProblemType.java
@@ -6,12 +6,12 @@ import lombok.Getter;
 public enum ProblemType {
 
     SYSTEM_ERROR("/system-error", "System-error"),
-    INVALID_DATA("/invalid-data", "Invalid data"),
-    INVALID_PARAMETER("/invalid-parameter", "Invalid parameter"),
-    MESSAGE_INCOMPREHESIBLE("/message-incomprehensible", "Message incomprehensible"),
-    RESOURCE_NOT_FUND("/resource-not-found", "Resource not found"),
-    ENTITY_IN_USE("/entity-in-use", "Entity in use"),
-    BUSINESS_ERROR("/business-error", "Business error");
+    INVALID_DATA("/invalid-data", "Invalid Data"),
+    INVALID_PARAMETER("/invalid-parameter", "Invalid Parameter"),
+    MESSAGE_INCOMPREHESIBLE("/message-incomprehensible", "Message Incomprehensible"),
+    RESOURCE_NOT_FUND("/resource-not-found", "Resource Not Found"),
+    ENTITY_IN_USE("/entity-in-use", "Entity In Use"),
+    BUSINESS_ERROR("/business-error", "Business Rule Violation");
 
     private String title;
     private String uri;
